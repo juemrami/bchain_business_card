de4f2b6693c07c3b8f6f6256ae5c96b9e0c8447c
Julio Ramirez <julio1031.jr@gmail.com>
2022-01-28 20:04:50 -0800

    fixed some scripts for new directories.
    also deleted the old directories

--- Contract/build.sh
+++ Contract/build.sh
@@ -2,6 +2,5 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-mkdir -p ../frontend/contract/
 mkdir -p ../frontend/contract/wasm32_out/
 cp target/wasm32-unknown-unknown/release/*.wasm ../frontend/contract/wasm32_out/contract.wasm
--- frontend/contract/dev-deploy.sh
+++ frontend/contract/dev-deploy.sh
@@ -5,7 +5,7 @@ set -e
 
 echo
 echo ---------------------------------------------------------
-echo "Step 0: Build the contract (may take a few seconds)"
+echo "Step 1: Build the contract (may take a few seconds)"
 echo ---------------------------------------------------------
 echo
 ./build-contract.sh
@@ -13,22 +13,16 @@ echo
 echo
 echo
 echo ---------------------------------------------------------
-echo "Step 1: Deploy the contract"
-echo
-echo "(edit scripts/1.dev-deploy.sh to deploy other contract)"
+echo "Step 2: Deploy the contract"
 echo ---------------------------------------------------------
 echo
 
-# uncomment out the line below to deploy the other example contract
-# near dev-deploy ./build/debug/simple.wasm
-
-# comment the line below to deploy the other example contract
-near dev-deploy contract.wasm
+near dev-deploy wasm32_out/contract.wasm
 
 echo
 echo
 echo ---------------------------------------------------------
-echo "Step 3: Prepare your environment for next steps"
+echo "Step 3: Prepare your environment"
 echo
 echo "(a) find the contract (account) name in the message above"
 echo "    it will look like this: [ Account id: dev-###-### ]"
@@ -38,8 +32,6 @@ echo "    see example below (this may not work on Windows)"
 echo
 echo ---------------------------------------------------------
 echo 'export CONTRACT=<dev-123-456>'
-# uncomment this line for a useful hint when using the singleton style
-echo "near call \$CONTRACT init --accountId \$CONTRACT"
 echo ---------------------------------------------------------
 echo
 
