52283725e06706dcb4dfa218731d46ed46330e54
Julio Ramirez <julio1031.jr@gmail.com>
2022-01-29 16:07:28 -0800

    Update build.sh

--- Contract/build.sh
+++ Contract/build.sh
@@ -2,5 +2,5 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-mkdir -p ../frontend/contract_out
-cp target/wasm32-unknown-unknown/release/*.wasm ../frontend/contract_out/contract.wasm
+mkdir -p ../next_frontend/contract_out
+cp target/wasm32-unknown-unknown/release/*.wasm ../next_frontend/contract_out/contract.wasm
