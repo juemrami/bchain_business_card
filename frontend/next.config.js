8d4136964169c5c8201195bb533eb12ca0b0b223
Julio Ramirez <julio1031.jr@gmail.com>
2022-02-07 20:04:59 -0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,11 @@
 const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
   reactStrictMode: true,
   webpack(config) {
     config.plugins.push(new WindiCSSWebpackPlugin());
